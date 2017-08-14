d6f405b18436d51ce9e59281f5c91ce4fa5a3ffc
rgarzonj <rgarzonj@yahoo.es>
2017-08-14 23:57:45 +0200

    Adding cluster file

--- run_cluster_experiment.sh
+++ run_cluster_experiment.sh
@@ -2,5 +2,6 @@
 #!/bin/sh
 export PATH="/home/usuaris/rgarzonj/anaconda3/bin:$PATH"
 source activate tensorflow_cpu
-python /home/usuaris/rgarzonj/github/LSTMs/DQN_LSTM_BlocksWorld.py
+cd /home/usuaris/rgarzonj/github/LSTMs
+python DQN_LSTM_BlocksWorld.py
 source deactivate tensorflow_cpu
